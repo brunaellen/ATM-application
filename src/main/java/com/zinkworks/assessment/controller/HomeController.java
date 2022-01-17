43e5f98ea744725c8426a701de842dc03872b149
brunaellen <brunaellen.souza@hotmail.com>
2022-01-17 21:20:47 +0000

    adding front-end (main page)

---
+++ src/main/java/com/zinkworks/assessment/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.zinkworks.assessment.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/AtmHome")
+public class HomeController {
+
+  @GetMapping("/home")
+  public String home() {
+    return "atmHome";
+  }
+}
